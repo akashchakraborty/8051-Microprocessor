d8927df13883fa9f5402015692d5d5fba773c057
akashchakraborty <akash.chak1999@gmail.com>
2022-02-06 12:03:29 +0530

    first commit

---
+++ AssemblyCodes/code1_MOV_ADD.asm
@@ -0,0 +1,7 @@
+MOV A,#01H;load value 55H in reg A
+MOV R0,A;LOAD THE VAL OF A INTO RO
+MOV R1,A;LOAD THE VAL OF RO INTO R1
+MOV R1,#02H
+MOV A, #25H;load 25H into A
+MOV R2, #35H;load 35H into R2
+ADD A, R2 ;add R2 to Accumulator
